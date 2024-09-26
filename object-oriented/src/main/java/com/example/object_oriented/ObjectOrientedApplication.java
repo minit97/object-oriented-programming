f0a476d85134ce0253b1225210a62b6ee1eaaa54
박현민 <dndb4902@naver.com>
2024-09-26 15:57:34 +0900

    # 객체 지향 프로그래밍 입문 - project setting

---
+++ object-oriented/src/main/java/com/example/object_oriented/ObjectOrientedApplication.java
@@ -0,0 +1,13 @@
+package com.example.object_oriented;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectOrientedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjectOrientedApplication.class, args);
+	}
+
+}
---
+++ object-oriented/src/test/java/com/example/object_oriented/ObjectOrientedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.object_oriented;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectOrientedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
