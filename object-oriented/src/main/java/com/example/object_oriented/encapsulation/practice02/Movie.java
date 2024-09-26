17d8866fbd658965b83f9e0cca8322304d118768
박현민 <dndb4902@naver.com>
2024-09-26 16:21:59 +0900

    refactor: 데이터를 가진 쪽에 기능 추가 및 필요한 파리미터 받기

--- object-oriented/src/main/java/com/example/object_oriented/encapsulation/practice02/Movie.java
+++ object-oriented/src/main/java/com/example/object_oriented/encapsulation/practice02/Movie.java
@@ -5,7 +5,9 @@ public class Movie {
     public static int NEW_RELEASE = 1;
     private int priceCode;
 
-    public int getPriceCode() {
-        return priceCode;
+    public int getFrequentRenterPoints(int daysRented) {
+        if(priceCode == NEW_RELEASE && daysRented > 1) return 2;
+        else return 1;
     }
+
 }
--- object-oriented/src/main/java/com/example/object_oriented/encapsulation/practice02/Rental.java
+++ object-oriented/src/main/java/com/example/object_oriented/encapsulation/practice02/Rental.java
@@ -5,7 +5,6 @@ public class Rental {
     private int daysRented;
 
     public int getFrequentRenterPoints() {
-        if(movie.getPriceCode() == Movie.NEW_RELEASE && daysRented > 1) return 2;
-        else return 1;
+        return movie.getFrequentRenterPoints(daysRented);
     }
 }
